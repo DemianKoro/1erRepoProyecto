7627d7c199f14a298d1357112cca9543a8d30bbc
Demian Koro <demian.koro@gmail.com>
2021-11-01 12:07:12 -0300

    corrigiendo DOM

--- js/login.js
+++ js/login.js
@@ -8,14 +8,13 @@ data.addEventListener("click",() =>{
     console.log(email.value);
     alert("Bienvenido/a " + email.value + "!");
     localStorage.setItem("email", email.value);
-    localStorage.setItem("password", password.value);
-    //email.value="";
-    //password.value="";
-    
+    localStorage.setItem("password", password.value);   
     let user = document.createElement("li");
     user.textContent = ("Bienvenido/a " + email.value);
 
     ulList.appendChild(user);
+    email.value="";
+    password.value="";
 
 })
 
